030593553134382c5c942ba5638256ce78c0a0aa
RichardDuclos <89917967+RichardDuclos@users.noreply.github.com>
2023-11-07 16:20:17 +0100

    first commit

---
+++ src/test/java/com/richardduclos/tierlist/TierListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardduclos.tierlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
